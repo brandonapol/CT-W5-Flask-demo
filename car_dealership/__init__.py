e2502a804e72620290413d95203cc2b257ec3fa4
Brandon Apol <brandonapol@cedarville.edu>
2021-06-22 20:13:11 -0800

    second day of work, committed while listening to Dev Lemons

--- car_dealership/__init__.py
+++ car_dealership/__init__.py
@@ -3,10 +3,20 @@ from config import Config
 from .site.routes import site
 from .authentication.routes import auth
 
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+# This was not explained in class
+from models import db as root_db
 
+app = Flask(__name__)
 
 app.register_blueprint(site)
 app.register_blueprint(auth)
 
 app.config.from_object(Config)
+# this also was not explained in class
+root_db.init_app(app)
+# I assume this has to do with the 'Flask Migrate -m "msg"' command in terminal as I suspect the init_app method was on line 17
+migrate = Migrate(app, root_db)
+
+import models
